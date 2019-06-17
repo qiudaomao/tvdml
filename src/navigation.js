33d479382104fbb1ba51dbd44a6e804a819db104
qiudaomao <qiudaomao@gmail.com>
2019-06-17 10:43:01 +0800

    use local TVDML

--- src/navigation.js
+++ src/navigation.js
@@ -48,17 +48,25 @@ export function handleRoute(routeName) {
 
       if (!documents.length) {
         // eslint-disable-next-line max-len
-        console.warn(`Navigation to route "${routeName}" ended without rendering any navigation record!`);
+        console.warn(
+          `Navigation to route "${routeName}" ended without rendering any navigation record!`,
+        );
       }
 
       if (documents.length > 1) {
         // eslint-disable-next-line max-len
-        console.warn(`Navigation to route "${routeName}" ended with rendering more than one navigation record!`);
+        console.warn(
+          `Navigation to route "${routeName}" ended with rendering more than one navigation record!`,
+        );
       }
 
       if (documents[0].route !== routeName) {
         // eslint-disable-next-line max-len
-        console.warn(`Navigation to route "${routeName}" ended with unexpected navigation document "${documents[0].route}"!`);
+        console.warn(
+          `Navigation to route "${routeName}" ended with unexpected navigation document "${
+            documents[0].route
+          }"!`,
+        );
       }
 
       return payload;
@@ -82,7 +90,7 @@ export function dismissRoute(routeName) {
 
 export function navigate(routeName, params, isRedirect = false) {
   if (!launched) {
-    throw new Error('Can\'t process navigation before app is launched');
+    throw new Error("Can't process navigation before app is launched");
   }
 
   const targetRoute = routes[routeName];
@@ -108,18 +116,17 @@ export function redirect(routeName, params) {
   return navigate(routeName, params, true);
 }
 
-Object
-  .keys(event)
-  .forEach((id) => {
-    const symbol = event[id];
-    const name = symbol.toString();
+Object.keys(event).forEach(id => {
+  const symbol = event[id];
+  const name = symbol.toString();
 
-    App[name] = (options) => {
-      console.info('Fired handler for app lifecycle', name, options);
-      if (name === 'onLaunch') launched = true;
-      broadcast(symbol, options);
-    };
-  });
+  if (typeof App.TVDML === 'undefined') App.TVDML = {};
+  App.TVDML[name] = options => {
+    console.info('Fired handler for app lifecycle', name, options);
+    if (name === 'onLaunch') launched = true;
+    broadcast(symbol, options);
+  };
+});
 
 subscribe('menu-item-select').pipe(({ menuItem, menuBar }) => {
   const routeValue = menuItem.getAttribute('route');
